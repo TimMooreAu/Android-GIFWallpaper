39ab0b344e9e631f2e0879d804038ab6429b6f0a
Ross <ross.t.pickett@gmail.com>
2015-04-22 23:24:34 +0930

    touch events :) kinda

---
+++ app/src/main/java/com/hathy/gifwallpaper/Touch.java
@@ -0,0 +1,19 @@
+package com.hathy.gifwallpaper;
+
+/**
+ * Created by Ross on 22/04/2015.
+ */
+public class Touch {
+    private float _x, _y;
+    public Touch(float x, float y){
+        _x = x;
+        _y = y;
+    }
+
+    public float get_x() {
+        return _x;
+    }
+    public float get_y() {
+        return _y;
+    }
+}
